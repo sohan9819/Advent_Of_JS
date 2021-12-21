052095d49d9fb0a2f9041b59d3f86aac04440f8f
sohan9819 <sohanshetty2001@gmail.com>
2021-12-21 16:47:10 +0530

    Challenge_4 Completed

---
+++ Challenge_4/app.js
@@ -0,0 +1,55 @@
+const keys = Array.from(document.querySelectorAll(".key"));
+const scoreDisplay = document.querySelector(".score");
+let activeKey;
+let score = 0;
+let randomIndex;
+
+keys.map((key) => {
+  key.classList.remove("jiggle");
+});
+
+randomIndex = keyActivation();
+
+keys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    // console.log(e.target.dataset.key);
+    keys[randomIndex].dataset.key === e.target.dataset.key
+      ? rightAns()
+      : wrongAns();
+  });
+});
+
+keys.forEach((key) => {
+  key.addEventListener("transitionend", () => {
+    key.classList.remove("rightAns");
+    key.classList.remove("wrongAns");
+  });
+});
+
+function keyActivation() {
+  randomIndex = Math.floor(Math.random() * 52);
+  keys[randomIndex].classList.add("jiggle");
+  return randomIndex;
+}
+
+function gameChecker(e) {
+  keys[randomIndex].dataset.key === e.key.toUpperCase()
+    ? rightAns()
+    : wrongAns();
+}
+
+function rightAns() {
+  keys[randomIndex].classList.add("rightAns");
+
+  keys[randomIndex].classList.remove("jiggle");
+  score++;
+  scoreDisplay.innerText = score;
+  randomIndex = keyActivation();
+}
+function wrongAns() {
+  keys[randomIndex].classList.add("wrongAns");
+  keys[randomIndex].classList.remove("jiggle");
+  randomIndex = keyActivation();
+}
+
+window.addEventListener("keypress", gameChecker);
